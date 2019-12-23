855710b6ba48b6e4da381630e65e04662ee0201f
Mikhail Chermensky <interstate80@yandex.ru>
2019-12-23 11:45:08 +0300

    Ver. Chng.

--- saa.py
+++ saa.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.7"
+__version__ = "0.1.0"
 __author__ = "Mikhail Chermenskiy <m.interstate@gmail.com>"
 
 import codecs
@@ -50,7 +50,8 @@ def setresdirpath():
     resdir_text['text'] = 'Папка для перемещения: %s' % resDir
     mv_butt['state'] = 'normal'
 
-def find():  # Поиск файлов
+# Поиск файлов
+def find():
     # global resDir
     # global f_amount
     listbox.delete(0, END)
@@ -180,9 +181,10 @@ def func_writelog(files):
         f.write(writestr)
     f.close()
 
+# получаем дату модификации файла
 def get_date(dfile):
     try:
-        return datetime.fromtimestamp(int(os.path.getmtime(dfile))) # получаем дату модификации файла
+        return datetime.fromtimestamp(int(os.path.getmtime(dfile)))
     except Exception as e:
         func_writelog(str(e))
 
@@ -247,9 +249,9 @@ m.add_cascade(label="?", menu=hm)
 hm.add_command(label="О программе", command=about)
 
 # ----------- ОСНОВНЫЕ ФРЕЙМЫ
-topFrame = Frame(root, height=80, bd=2, relief=GROOVE)
-bottomFrameLeft = Frame(root, width=300, bd=2, relief=GROOVE)
-bottomFrameRight = Frame(root, bd=2, relief=GROOVE)
+topFrame = Frame(root, height=80, bd=2, relief='GROOVE')
+bottomFrameLeft = Frame(root, width=300, bd=2, relief='GROOVE')
+bottomFrameRight = Frame(root, bd=2, relief='GROOVE')
 
 # ----------- ПОЛЯ ДЛЯ ДАТЫ
 day_ent = Entry(topFrame, width=10)
@@ -257,13 +259,13 @@ month_ent = Entry(topFrame, width=10)
 year_ent = Entry(topFrame, width=10)
 
 # ----------- ОСНОВНОЙ ЛИСТБОКС
-listbox = Listbox(bottomFrameRight, height=340, width=300, selectmode=EXTENDED)
+listbox = Listbox(bottomFrameRight, height=340, width=300, selectmode='EXTENDED')
 
 # ----------- ТЕКСТОВЫЕ МЕТКИ
-dir_text = Label(topFrame, height=1, justify=LEFT, text='Папка для поиска не выбрана...')
-resdir_text = Label(topFrame, height=1, justify=LEFT, text='Папка для переноса не выбрана...')
-count_text = Label(topFrame, height=1, justify=LEFT, text='Найдено файлов: ')
-info_text = Label(topFrame, height=1, justify=LEFT, text='')
+dir_text = Label(topFrame, height=1, justify='LEFT', text='Папка для поиска не выбрана...')
+resdir_text = Label(topFrame, height=1, justify='LEFT', text='Папка для переноса не выбрана...')
+count_text = Label(topFrame, height=1, justify='LEFT', text='Найдено файлов: ')
+info_text = Label(topFrame, height=1, justify='LEFT', text='')
 
 # ----------- КНОПКИ
 setdir_butt = Button(bottomFrameLeft, text='Исх. папка', width=10, command=setdirpath)
@@ -274,8 +276,8 @@ mv_butt = Button(bottomFrameLeft, text='Переместить', width=10, comma
 # ----------- РАДИОКНОПКИ
 words_var = IntVar()
 words_var.set(0)
-wrad0 = Radiobutton(topFrame, text='Без словаря', variable=words_var, value=0, command=wrad_change).grid(row=3, column=0, sticky=W)
-wrad1 = Radiobutton(topFrame, text='Со словарем', variable=words_var, value=1, command=wrad_change).grid(row=3, column=1, sticky=W)
+wrad0 = Radiobutton(topFrame, text='Без словаря', variable=words_var, value=0, command=wrad_change).grid(row=3, column=0, sticky='W')
+wrad1 = Radiobutton(topFrame, text='Со словарем', variable=words_var, value=1, command=wrad_change).grid(row=3, column=1, sticky='W')
 
 # ----------- СКРОЛЛБАР ДЛЯ ЛИСТБОКСА
 scrollbar = Scrollbar(listbox)
@@ -287,18 +289,18 @@ topFrame.pack(side='top', fill='both')
 bottomFrameLeft.pack(side='left', fill='y')
 bottomFrameRight.pack(side='left', fill='both', expand=1)
 
-Label(topFrame, text='День:').grid(row=0, column=0, sticky=W)
-Label(topFrame, text='Месяц:').grid(row=1, column=0, sticky=W)
-Label(topFrame, text='Год:').grid(row=2, column=0, sticky=W)
+Label(topFrame, text='День:').grid(row=0, column=0, sticky='W')
+Label(topFrame, text='Месяц:').grid(row=1, column=0, sticky='W')
+Label(topFrame, text='Год:').grid(row=2, column=0, sticky='W')
 
 day_ent.grid(row=0, column=1)
 month_ent.grid(row=1, column=1)
 year_ent.grid(row=2, column=1)
 
-dir_text.grid(row=0, column=2, sticky=W)
-resdir_text.grid(row=1, column=2, sticky=W)
-count_text.grid(row=2, column=2, sticky=W)
-info_text.grid(row=2, column=3, sticky=E)
+dir_text.grid(row=0, column=2, sticky='W')
+resdir_text.grid(row=1, column=2, sticky='W')
+count_text.grid(row=2, column=2, sticky='W')
+info_text.grid(row=2, column=3, sticky='E')
 
 go_butt.pack(side='top')
 mv_butt.pack(side='top')
