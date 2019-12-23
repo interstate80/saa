b8b41233167b4e2911eba860d0c7de4dc0423bc5
Mikhail Chermensky <interstate80@yandex.ru>
2019-12-23 11:53:51 +0300

    1

---
+++ gui.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.1.0"
+__author__ = "Mikhail Chermenskiy <m.interstate@gmail.com>"
+
+from flexx import flx
+
+class MainWindow(self):
+    def __init__(self):
+        super(MainWindow,self).__init__()
